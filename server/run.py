a72d8633d6ab5c87a50295995e8752c1670435be
Rick Lewis <rick@Ricks-MacBook-Pro.local>
2020-11-22 23:17:03 -0600

    split server into separate desktop and pedal-pi versions

--- server/run.py
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/python3
-from app import flaskapp, db
-
-meta = db.metadata
-for table in reversed(meta.sorted_tables):
-    db.session.execute(table.delete())
-db.session.commit()
-
-flaskapp.run(debug=True)
